fe247b68eaf35dc08cc001eefe3800ef7e4a9efa
DavidLibera <david.libera@mail.mcgill.ca>
2017-03-12 23:01:24 -0400

    Computing global matrix, displacement and elemental displacement and stress, Now onto colormap NOTE: Problem is its only limited to 4 nodes for now

--- MeshWorks/FEM.cpp
+++ MeshWorks/FEM.cpp
@@ -56,6 +56,15 @@ void FEM::PrintMatrix(double** &a, int row, int col)
 	std::cout << "-------------------------------" << std::endl;
 }
 
+void FEM::PrintVector(double* &a, int row) {
+	std::cout << "-------------------------------" << std::endl;
+	int i, j;
+	for (i = 0; i < row; i++) {
+		std::cout << a[i] << " " << std::endl;
+	}
+	std::cout << "-------------------------------" << std::endl;
+}
+
 // Setting
 void FEM::setProps() {
 	std::cout << "FEM::setProps()" << std::endl;
@@ -118,6 +127,11 @@ void FEM::setDMatrix() {
 	}
 }
 
+void FEM::setBCs() {
+	Isol[0][0] = 1; Isol[0][1] = 3; Isol[0][2] = 4;
+	f[3] = -1225; // Fy at node 2 is 1225 
+}
+
 // Computing
 
 void FEM::computeKMatrix() {
@@ -147,6 +161,7 @@ void FEM::computeKMatrix() {
 	int n1, n2, n3;
 	double x1, y1, z1, x2, y2, z2, x3, y3, z3;
 	double b1, b2, b3, c1, c2, c3, A;
+
 	for (int element = 0; element < nFaces; element++) {
 
 		// Extracting node number from matConn matrix
@@ -155,9 +170,10 @@ void FEM::computeKMatrix() {
 		n3 = matConn[element][2];
 
 		// Using node number from above to access x,y coordinates (Note: index starts is 1 less for matrices, hence the -1)
-		x1 = matNodes[n1-1][0]; y1 = matNodes[n1-1][1];
-		x2 = matNodes[n2-1][0]; y2 = matNodes[n2-1][1];
-		x3 = matNodes[n3-1][0]; y3 = matNodes[n3-1][1];
+		x1 = matNodes[n1 - 1][0]; y1 = matNodes[n1 - 1][1];
+
+		x2 = matNodes[n2 - 1][0]; y2 = matNodes[n2 - 1][1];
+		x3 = matNodes[n3 - 1][0]; y3 = matNodes[n3 - 1][1];
 
 		// Computing area of element
 		A = 0.5*((x2*y3 - x3*y2) + (x3*y1 - x1*y3) + (x1*y2 - x2*y1)); // needs an absolute sign
@@ -167,13 +183,12 @@ void FEM::computeKMatrix() {
 		c1 = x3 - x2; c2 = x1 - x3; c3 = x2 - x1;
 
 		// Setting matrix B 
-		B[0][0] = b1/(2*A); B[0][2] = b2/(2*A); B[0][4] = b3/(2*A); 
-		B[1][1] = c1/(2*A); B[1][3] = c2/(2*A); B[1][5] = c3/(2*A);
+		B[0][0] = b1 / (2 * A); B[0][2] = b2 / (2 * A); B[0][4] = b3 / (2 * A);
+		B[1][1] = c1 / (2 * A); B[1][3] = c2 / (2 * A); B[1][5] = c3 / (2 * A);
 		B[2][1] = b1 / (2 * A); B[2][3] = b2 / (2 * A); B[2][5] = b3 / (2 * A);
 		B[2][0] = c1 / (2 * A); B[2][2] = c2 / (2 * A); B[2][4] = c3 / (2 * A);
 
-		// Performing Ke computation 
-		// Ke = A*t*BT*D*B 
+		// Performing Ke computation (Ke = A*t*BT*D*B )
 		GLKMatrixLib::Transpose(B, Brow, Bcol, BT);
 		GLKMatrixLib::Mul(BT, D, 6, 3, 3, temp);		// Matrix Multiplication seperate TEDIOUS
 		GLKMatrixLib::Mul(temp, B, 6, 3, 6, Ke);
@@ -183,13 +198,11 @@ void FEM::computeKMatrix() {
 				Ke[i][j] = t*A*Ke[i][j];
 			}
 		}
-
+		
 		// scattering results into global matrix
 		scatter(Ke, K, n1, n2, n3);
 
 	}
-	
-	PrintMatrix(K, Krow, Kcol);
 
 }
 
@@ -200,23 +213,23 @@ void FEM::scatter(double** &Ke, double** &K, int n1, int n2, int n3) {
 	for (int i = 0; i < Kerow; i++) {
 
 		switch (i) {
-		case 1: row = 2 * n1 - 2; break;
-		case 2: row = 2 * n1 - 1; break;
-		case 3: row = 2 * n2 - 2; break;
-		case 4: row = 2 * n2 - 1; break;
-		case 5: row = 2 * n3 - 2; break;
-		case 6: row = 2 * n3 - 1; break;
+		case 0: row = 2 * n1 - 2; break;
+		case 1: row = 2 * n1 - 1; break;
+		case 2: row = 2 * n2 - 2; break;
+		case 3: row = 2 * n2 - 1; break;
+		case 4: row = 2 * n3 - 2; break;
+		case 5: row = 2 * n3 - 1; break;
 		}
 
 		for (int j = 0; j < Kecol; j++) {
 
 			switch (j) {
-			case 1: col = 2 * n1 - 2; break;
-			case 2: col = 2 * n1 - 1; break;
-			case 3: col = 2 * n2 - 2; break;
-			case 4: col = 2 * n2 - 1; break;
-			case 5: col = 2 * n3 - 2; break;
-			case 6: col = 2 * n3 - 1; break;
+			case 0: col = 2 * n1 - 2; break;
+			case 1: col = 2 * n1 - 1; break;
+			case 2: col = 2 * n2 - 2; break;
+			case 3: col = 2 * n2 - 1; break;
+			case 4: col = 2 * n3 - 2; break;
+			case 5: col = 2 * n3 - 1; break;
 			}
 			K[row][col] = K[row][col] + Ke[i][j];
 			
@@ -229,6 +242,137 @@ void FEM::scatter(double** &Ke, double** &K, int n1, int n2, int n3) {
 		}
 	}
 
+}
+
+void FEM::scatterKmod(double** &K, double** &Kmod, double** &Isol) {
+
+	// WARNING: IT IS NOT GENERAL ONLY FOR 4 node case 
+
+	// Entries of Isol represent rows and cols that will be extracted
+	int n = 3; //sizeof(Isol[0]) / sizeof(Isol[0][0]);
+	std::cout << "n:(for scatterKmod)" << n << std::endl;
+	int row, col;
+
+	//Creating Kmod 
+	for (int i = 0; i < n; i++) {
+		
+		// safety switch 
+		if (i == 10) break;
+		
+		switch (i) {
+			case 0: row = Isol[0][0]-1; break;
+			case 1: row = Isol[0][1]-1; break;
+			case 2: row = Isol[0][2]-1; break;
+		}
+		for (int j = 0; j < n; j++) {
+
+			switch (j) {
+				case 0: col = Isol[0][0]-1; break;
+				case 1: col = Isol[0][1]-1; break;
+				case 2: col = Isol[0][2]-1; break;
+			}
+
+			Kmod[i][j] = K[row][col];
+
+		}
+
+	}
+
+}
+
+void FEM::scatterfmod(double* &f, double* &fmod, double** &Isol) {
+
+	// WARNING! : Only valid for 4 nodes 
+
+	int n = 3; // n is the number of columns in Isol 
+	int row;
+
+	for (int i = 0; i < n; i++) {
+
+		// safety switch 
+		if (i == 10) break;
+
+		switch (i) {
+		case 0: row = Isol[0][0] - 1; break;
+		case 1: row = Isol[0][1] - 1; break;
+		case 2: row = Isol[0][2] - 1; break;
+		}
+		fmod[i] = f[row];
+	}
+}
+
+void FEM::scatterBackDisplacements(double* &dmod, double* &d, double** &Isol) {
+
+	// WARNING! : Only valid for 4 nodes 
+
+	int n = 3; // n is the number of columns in Isol 
+	int row;
+
+	for (int i = 0; i < n; i++) {
+
+		// safety switch 
+		if (i == 10) break;
+
+		switch (i) {
+		case 0: row = Isol[0][0] - 1; break;
+		case 1: row = Isol[0][1] - 1; break;
+		case 2: row = Isol[0][2] - 1; break;
+		}
+		d[row] = dmod[i];
+	}
+}
+
+void FEM::computeStress() {
+
+	int n1, n2, n3;
+	double b1, b2, b3, c1, c2, c3, x1, x2, x3, y1, y2, y3,A;
+
+	for (int element = 0; element < nFaces; element++) {
+		n1 = matConn[element][0];
+		n2 = matConn[element][1];
+		n3 = matConn[element][2];
+
+		de[0] = d[2 * n1 - 2];
+		de[1] = d[2 * n1 - 1];
+		de[2] = d[2 * n2 - 2];
+		de[3] = d[2 * n2 - 1];
+		de[4] = d[2 * n3 - 2];
+		de[5] = d[2 * n3 - 1];
+
+		// RECOMPUTE B MATRIX 
+		// Using node number from above to access x,y coordinates (Note: index starts is 1 less for matrices, hence the -1)
+		x1 = matNodes[n1 - 1][0]; y1 = matNodes[n1 - 1][1];
+		x2 = matNodes[n2 - 1][0]; y2 = matNodes[n2 - 1][1];
+		x3 = matNodes[n3 - 1][0]; y3 = matNodes[n3 - 1][1];
+
+		// Computing area of element
+		A = 0.5*((x2*y3 - x3*y2) + (x3*y1 - x1*y3) + (x1*y2 - x2*y1)); // needs an absolute sign
+
+																	   // Computing elements of matrix B
+		b1 = y2 - y3; b2 = y3 - y1; b3 = y1 - y2;
+		c1 = x3 - x2; c2 = x1 - x3; c3 = x2 - x1;
+
+		// Setting matrix B 
+		B[0][0] = b1 / (2 * A); B[0][2] = b2 / (2 * A); B[0][4] = b3 / (2 * A);
+		B[1][1] = c1 / (2 * A); B[1][3] = c2 / (2 * A); B[1][5] = c3 / (2 * A);
+		B[2][1] = b1 / (2 * A); B[2][3] = b2 / (2 * A); B[2][5] = b3 / (2 * A);
+		B[2][0] = c1 / (2 * A); B[2][2] = c2 / (2 * A); B[2][4] = c3 / (2 * A);
+
+		// Computing Stress
+		GLKMatrixLib::Mul(D, B, 3, 3, 6, temp2);
+		GLKMatrixLib::Mul(temp2, de, 3, 6, sige);
+
+		double vonMises = computeVonMises();
+
+	}
+
+}
+
+double FEM::computeVonMises() {
+
+	// For 2D problems only
+
+	return sqrt( (pow((sige[0] - sige[1]), 2) + pow(sige[0], 2) + pow(sige[1], 2) + 6 * pow(sige[2], 2))/2);
 }
 
 // Getting 
@@ -276,14 +420,26 @@ void FEM::Create() {
 	GLKMatrixLib::CreateMatrix(matConn, matConnRow, matConnCol);
 	GLKMatrixLib::CreateMatrix(B, Brow, Bcol);
 	GLKMatrixLib::CreateMatrix(D, Drow, Dcol);
-	GLKMatrixLib::CreateMatrix(f, frow, fcol);
-	GLKMatrixLib::CreateMatrix(d, drow, dcol);
+	CreateVector(f, frow);
+	CreateVector(d, drow);
 	GLKMatrixLib::CreateMatrix(K, Krow, Kcol);
 	GLKMatrixLib::CreateMatrix(Ke, Kerow, Kecol);
+
+	// Solving System of Equations
+	GLKMatrixLib::CreateMatrix(Isol, Isolrow, Isolcol);
+	GLKMatrixLib::CreateMatrix(Kmod, Kmodrow, Kmodcol);
+	CreateVector(fmod, fmodrow);
+	CreateVector(dmod, dmodrow);
+
+	//Solving stresses
+	CreateVector(sige, sigerow);
+	CreateVector(de, derow);
 
 	// Miscallenous
 	GLKMatrixLib::CreateMatrix(BT, BTrow, BTcol);
 	GLKMatrixLib::CreateMatrix(temp, trow, tcol);
+	GLKMatrixLib::CreateMatrix(temp2, t2row, t2col);
+
 }
 
 void FEM::Destroy() {
@@ -293,14 +449,36 @@ void FEM::Destroy() {
 	GLKMatrixLib::DeleteMatrix(matConn, matConnRow, matConnCol);
 	GLKMatrixLib::DeleteMatrix(B, Brow, Bcol);
 	GLKMatrixLib::DeleteMatrix(D, Drow, Dcol);
-	GLKMatrixLib::DeleteMatrix(f, frow, fcol);
-	GLKMatrixLib::DeleteMatrix(d, drow, dcol);
+	DeleteVector(f);
+	DeleteVector(d);
 	GLKMatrixLib::DeleteMatrix(K, Krow, Kcol);
 	GLKMatrixLib::DeleteMatrix(Ke, Kerow, Kecol);
+	
+	// Solving system of equations
+	GLKMatrixLib::DeleteMatrix(Isol, Isolrow, Isolcol);
+	GLKMatrixLib::DeleteMatrix(Kmod, Kmodrow, Kmodcol);
+	DeleteVector(fmod);
+	DeleteVector(dmod);
+
+	//Solving stresses
+	DeleteVector(sige);
+	DeleteVector(de);
 
 	// Miscallenous
 	GLKMatrixLib::DeleteMatrix(BT, BTrow, BTcol);
 	GLKMatrixLib::DeleteMatrix(temp, trow, tcol);
+	GLKMatrixLib::DeleteMatrix(temp2, t2row, t2col);
+}
+
+void FEM::CreateVector(double* &ptr, int ptrsize) {
+	ptr = new double[ptrsize];
+	for (int i = 0; i < ptrsize; i++) {
+		ptr[i] = 0.0;
+	}
+}
+
+void FEM::DeleteVector(double* &ptr) {
+	delete[] ptr; 
 }
 
 
@@ -320,11 +498,20 @@ void FEM::MainFunction() {
 	//Compute K matrix
 	computeKMatrix();
 
-	// Print
-	PrintMatrix(matNodes, matNodesRow, matNodesCol);
-	PrintMatrix(matConn, matConnRow, matConnCol);
-	PrintMatrix(D, Drow, Dcol);
+	//Set BCS
+	setBCs();
 
+	//Modify System of Equations based on BCs
+	scatterKmod(K, Kmod, Isol);
+	scatterfmod(f, fmod, Isol);
+
+	//Solve system of equations
+	GLKMatrixLib::GaussSeidelSolver(Kmod, fmod, 3, dmod,1e-6);
+	scatterBackDisplacements(dmod, d, Isol);
+
+	//Compute Stresses
+	computeStress();
+	
 	// Deallocate memory before exiting
 	Destroy();
 
--- MeshWorks/FEM.h
+++ MeshWorks/FEM.h
@@ -14,16 +14,23 @@ public:
 	// printing 
 	void printNodeCoord();
 	void PrintMatrix(double** &a, int row, int col);
+	void PrintVector(double* &a, int row);
 
 	// Setting
 	void setProps();
 	void setNodeMatrix();
 	void setConnMatrix();
 	void setDMatrix(); // strain-stress matrix
+	void setBCs();
 
 	// Computing
 	void computeKMatrix();
 	void scatter(double** &Ke, double** &K, int n1, int n2, int n3);
+	void scatterKmod(double** &K, double** &Kmod, double** &Isol);
+	void scatterfmod(double* &f, double* &fmod, double** &Isol);
+	void scatterBackDisplacements(double* &dmod, double* &d, double** &Isol);
+	void computeStress();
+	double computeVonMises();
 
 	// Getting functions
 	int getNumberOfNodes();
@@ -32,6 +39,8 @@ public:
 	// Setting memory
 	void Create();
 	void Destroy();
+	void CreateVector(double* &ptr, int ptrsize);
+	void DeleteVector(double* &ptr);
 
 	// PostProcessing
 	void colorFaces();
@@ -45,19 +54,30 @@ public:
 	int nFaces = getNumberOfFaces();
 	int nDOF = nNodes * 2;
 
-	// Matrices
+	// Matrices/Vectors
 	double** matNodes; int matNodesRow = nNodes, matNodesCol = 2;
 	double** matConn;  int matConnRow = nFaces, matConnCol = 3;
 	double** D; int Drow = 3, Dcol = 3;
 	double** B; int Brow = 3, Bcol = 6;
-	double** f; int frow = nDOF, fcol = 1; 
-	double** d; int drow = nDOF, dcol = 1;
+	double* f; int frow = nDOF;
+	double* d; int drow = nDOF;
 	double** K; int Krow = nDOF, Kcol = nDOF;
 	double** Ke; int Kerow = 6, Kecol = 6;
+
+	// Matrices/Vectors for solving systems 
+	double** Isol; int Isolrow = 1, Isolcol =3; // THESE DIMENSIONS AFFECT THE NEXT MOD MATRICES for general case
+	double** Kmod; int Kmodrow = 3, Kmodcol = 3;
+	double* fmod; int fmodrow = 3;
+	double* dmod; int dmodrow = 3;
+
+	//Matrices/Vectors for stress strain elemental
+	double* sige; int sigerow = 3; // sige = stress element 
+	double* de; int derow = 6;     // de = displacement element  
 
 	//miscallenous matrices
 	double** BT;			int BTrow = Bcol, BTcol = Brow;
 	double**temp;			int trow = 6, tcol = 3;
+	double**temp2;          int t2row = 3, t2col = 6;
 
 };
 
